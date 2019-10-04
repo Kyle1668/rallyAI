f3dbf98d9c9511ea6304d3898a5016e93abb926e
kyle1668 <kyledevinobrien1@gmail.com>
2019-10-04 13:00:39 -0700

    added NOT NULL constraints

--- database/init.sql
+++ database/init.sql
@@ -1,9 +1,9 @@
 CREATE TABLE stocks
 (
     symbol VARCHAR (10) PRIMARY KEY,
-    company_name VARCHAR (50),
-    market_date DATE,
-    closing_price REAL,
+    company_name VARCHAR (50) NOT NULL,
+    market_date DATE NOT NULL,
+    closing_price REAL NOT NULL,
     opening_price REAL,
     highest_price REAL,
     lowest_price REAL,
