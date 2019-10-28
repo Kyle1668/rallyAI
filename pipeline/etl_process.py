45c42b0920bd7ad9f1ac09eec5e6c0da919fd0d6
kyle1668 <kyledevinobrien1@gmail.com>
2019-10-28 12:01:32 -0700

    added CSV wriitng to ETL pipeline and made the master node address an env var

--- pipeline/etl_process.py
+++ pipeline/etl_process.py
@@ -1,6 +1,5 @@
 """
 RalliAI - UCSC CSE 115A - Fall 2019
-Kyle O'Brien - kdobrien@ucsc.edu
 """
 
 import os
@@ -12,7 +11,7 @@ from pyspark.sql.types import (StructField, StructType, StringType,
 
 def init_postgres_connection():
     """Init the connection to the Postgres database using env vars
-    
+
     Returns:
         [cursor] -- [A cursor for the DB connection for running queries against]
     """
@@ -30,7 +29,7 @@ def init_postgres_connection():
 
 def write_dataframe_to_postgres(arg_data_frame, table_name):
     """Loads an argues Spark dataframe to the PG database.
-    
+
     Arguments:
         arg_data_frame {DataFrame} -- The dataframe who's contents will be written to the DB
         table_name {String} -- The DB table that will be written to
@@ -43,9 +42,9 @@ def write_dataframe_to_postgres(arg_data_frame, table_name):
 
         connection_string = f"jdbc:postgresql://{db_address}/{table_name}"
         connection_properties = {
-            "password": "docker",
             "driver": "org.postgresql.Driver",
-            "user": "docker"
+            "user": db_user,
+            "password": db_password,
         }
 
         arg_data_frame.write.jdbc(url=db_address,
@@ -64,24 +63,24 @@ def write_dataframe_to_postgres(arg_data_frame, table_name):
 
 
 def generate_data_frame_from_csv(csv_file_path):
-    """This method creates a DataFrame schema and then reads 
+    """This method creates a DataFrame schema and then reads
        the values from an argued CSV file into a DataFrame.
-    
+
     Arguments:
         csv_file_path {String} -- The relative path tot he CSV file to be read.
-    
+
     Returns:
         DataFrame -- The new Spark DataFrame containign the files contents.
     """
     data_schema = [
         StructField("ticker", StringType(), True),
-        StructField("open", FloatType()),
-        StructField("close", FloatType()),
-        StructField("adj_close", FloatType()),
-        StructField("low", FloatType()),
-        StructField("high", FloatType()),
-        StructField("volume", FloatType()),
-        StructField("date", DateType())
+        StructField("open", FloatType(), True),
+        StructField("close", FloatType(), True),
+        StructField("adj_close", FloatType(), True),
+        StructField("low", FloatType(), True),
+        StructField("high", FloatType(), True),
+        StructField("volume", FloatType(), True),
+        StructField("date", DateType(), True)
     ]
 
     final_struct = StructType(fields=data_schema)
@@ -100,23 +99,29 @@ def generate_data_frame_from_csv(csv_file_path):
 
 
 if __name__ == "__main__":
-    """Program Driver
-    """
+    # Init new Spark application session.
+    spark_master = os.environ["SPARK_MASTER"]
     spark = SparkSession.builder \
-        .master("local[4]") \
+        .master(spark_master) \
         .appName("rallyai_etl_pipeline") \
         .config("spark.jars", "/rallyai/spark-etl-pipeline/jars/postgresql-42.2.8.jar") \
+        .config("spark.driver.memory", "7g") \
         .getOrCreate() \
 
+    # Init new loggers
     log4jLogger = spark._jvm.org.apache.log4j
     log = log4jLogger.LogManager.getLogger(__name__)
 
+    # Read the raw dataset from the CSV file
     raw_dataset_path = os.environ["CSV_FILE_PATH"]
     data_frame = generate_data_frame_from_csv(raw_dataset_path)
 
     # Print the schema to the console
     data_frame.printSchema()
 
-    results = spark.sql("SELECT * FROM stocks")
+    results = spark.sql(
+        "SELECT * FROM stocks WHERE symbol IS NOT  NULL AND date IS NOT NULL")
     db_connection_cursor = init_postgres_connection()
     write_dataframe_to_postgres(results, "stocks")
+
+    results.write.csv("model_training_data", mode="overwrite")
