33a56869227520469b3df0969cb8d3a6341dedc9
mrunknown.892@gmail.com <jnguy243@ucsc.edu>
2019-11-12 17:36:37 -0800

    adding babel and knex, got basic request finished

--- api/index.js
+++ api/index.js
@@ -1,17 +1,56 @@
-const express = require("express");
-const express_graphql = require("express-graphql");
-const { buildSchema } = require("graphql");
+import express from 'express';
+import express_graphql from 'express-graphql';
+import {
+  buildSchema
+} from 'graphql';
+
+// Enviroment variables
+import dotenv from 'dotenv';
+dotenv.config();
+
+var knex = require('knex')({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'joseph',
+    password : 'password',
+    database : 'rallyai'
+  }
+});
 
 // GraphQL schema
 const schema = buildSchema(`
-    type Query {
-        message: String
-    }
+  type Stock {
+    id: Int
+    symbol: String
+    company_name: String
+    market_date: String
+    closing_price: Float
+    opening_price: Float
+    highest_price: Float
+    lowest_price: Float
+    volume_in_millions: String
+  }
+
+  type Query {
+    statusCode: Int
+    error: Boolean
+    results(stockSymbol: String): [Stock]
+  }
 `);
+
+const getDataFromSymbol = async (symbol) => {
+  const data = await knex('stocks').where('symbol', symbol);
+  return data;
+}
 
 // Root resolver
 const root = {
-  message: () => "Hello World!"
+  results: async ({stockSymbol}) => {
+    const returnedData = await getDataFromSymbol(stockSymbol);
+    console.log(returnedData);
+    return returnedData;
+  }
 };
 
 // Create an express server and a GraphQL endpoint
